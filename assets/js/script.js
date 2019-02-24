8e26779f4ff15ad96467dc1d68738ddca02e4069
AmauryD19 <amaurydotrice@gmail.com>
2019-02-24 15:09:12 +0100

    Création variables

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* LES VARIABLES */
+
+/* Les variables de bases*/
+let affichage = document.getElementById('affichage');
+let clic = document.getElementById('clic');
+let multiplier = document.getElementById('multiplier');
+
+/* Les variables des bonus*/
+
+/* Les variables des prix des bonus */
+
+
+/* Les variables infos */
+
+let score = 0;
+let multiplicateur = 1;
+
+/* LES FONCTIONS */

c264aaa721bf287b1828276ba7e46b69e6c6aaf7
Amaury Dotrice <amaurydotrice@gmail.com>
2019-02-25 11:41:37 +0100

    beginScriptClicker

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+/* get Element */
+click = document.getElementById('click');
+display = document.getElementById('display');
+multiplicator = document.getElementById('multiplicator');
+autoclick = document.getElementById('autoclick');
+
+/* valeurs de base */
+
+let score = 0;
+let clickValue = 1;
+let multiplier = 1;
+
+/* fonctions */
+    /* les displays */
+function displayScore() {
+    display.innerHTML = score;
+}
+
+function displayMultiplicator() {
+    multiplicator.value = 'Multiplier x' + multiplier;
+}
+    /* les scores*/
+function clickScore() {
+    score += clickValue;
+    displayScore();
+}
+
+function multiplicatorScore() {
+    multiplier += 1;
+    displayScore();
+    displayMultiplicator();
+}
+/* appels des fonctions de base */
+click.addEventListener('click', clickScore);
+displayScore();
+displayMultiplicator(); 

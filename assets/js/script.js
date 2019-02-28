8a6ab2de487335078308327564bf39b4416363de
AmauryD19 <amaurydotrice@gmail.com>
2019-02-28 20:31:09 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ let multiplier = document.getElementById('multiplier');
 let autoclicker = document.getElementById('autoclicker');
 let bonus = document.getElementById('bonus');
 
-let score = 0;
+let score = 5000;
 let valeurClick = 1;
 let multiplicateur = 1;
 let prixMultipicateur= 50;
 let prixAutoclicker = 200;
 let prixBonus = 5000;
-let bonusTime = 0;
+let bonusTimer = 30;
 
 function augmenterMultiplicateur() {
     if (score >= prixMultipicateur) {
@@ -53,6 +53,41 @@ function bonusTEST() {
     }
 }*/
 
+/*function bonusTEST() {
+    score -= prixBonus;
+    bonusTimer = setInterval(function(){
+        score += 2*valeurClick;
+        bonusTimer--;
+        if (bonusTimer>0) {
+            bonus.innerHTML = bonusTimer;
+            console.log(bonusTimer);
+        }
+        if (bonusTEST<=0) {
+            bonusTEST();
+        }
+    }, 1000);
+}*/
+
+function bonusTEST() {
+    if(score>=prixBonus) {
+        score -= prixBonus;
+        let scoreBonus = 2*valeurClick
+        score += scoreBonus;
+        bonusTimer--;
+        console.log("look "+scoreBonus);
+        bonus.innerHTML = bonusTimer;
+        afficherScore();
+        if (bonusTimer===0) {
+            bonusOver();
+        }
+    }
+}
+
+function bonusOver(){
+    score += valeurClick;
+    afficherScore();
+}
+
 function augmenterScore() {
     score += valeurClick;
     console.log(score);
@@ -67,4 +102,4 @@ function afficherScore() {
 click.addEventListener('mousedown', augmenterScore);
 multiplier.addEventListener('mousedown', augmenterMultiplicateur);
 autoclicker.addEventListener('mousedown', autoclickerTEST);
-//bonus.addEventListener('mousedown', bonusTEST);
+bonus.addEventListener('mousedown', bonusTEST);
